8965b3903cff797b7c6959c213bdb88aac20c091
techcodie <bahetianshh@gmail.com>
2026-02-15 11:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (28.62%) - LeetSync

---
+++ 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.py
@@ -0,0 +1,16 @@
+from fractions import Fraction
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        s=""
+        num=eval(expression)
+        num1=int(num)
+        print(num1)
+        if num1==num:
+            s=str(num1)
+            s+='/1'
+        else:
+            res=Fraction(num).limit_denominator()
+            print(res)
+            s=str(res)
+        return s
+        
